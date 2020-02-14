b41921e7e7da99ac3484dee48d3aa53542fa482f
Sean Hickman <sean@grumply.com>
2020-02-13 23:37:21 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, pure-default, pure-json, stdenv }:
 mkDerivation {
   pname = "pure-try";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base pure-default pure-json ];
   homepage = "github.com/grumply/pure-try";
